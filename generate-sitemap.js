37dc7ce1acca06ff08b882a9b49c91873648a464
Ribai21 <mohammedribai21601@gmail.com>
2025-08-27 19:56:48 +0530

    seo fixed

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -5,8 +5,11 @@ const baseUrl = "https://alphaarena.netlify.app";
 
 const urls = [
   { url: "/", changefreq: "daily", priority: 1.0 },
-
   { url: "/about", changefreq: "monthly", priority: 0.8 },
+  { url: "/programs", changefreq: "weekly", priority: 0.7 },
+  { url: "/plans", changefreq: "weekly", priority: 0.7 },
+  { url: "/contact", changefreq: "monthly", priority: 0.6 },
+  { url: "/testimonials", changefreq: "monthly", priority: 0.6 },
 ];
 
 const sitemapStream = new SitemapStream({ hostname: baseUrl });
